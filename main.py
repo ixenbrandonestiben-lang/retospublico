e261dfc5c6f377ebf6f4cc930abba80dd93669fb
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-03-12 08:05:30 -0600

    feat: :sparkles: separador y organizacion
    agrege un separador y organice la presentacion en pantalla

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 def calcular ():
-    proteina = float(input("ingrese los gramos de proteina:"))                 
+    separador()
+    print(input("ingrese el nombre del alimento: "))
+    separador()
+    proteina = float(input("ingrese los gramos de proteina:"))  
+    separador()               
     carbohidratos = float(input("ingrese los gramos de carbohidratos: "))
+    separador() 
     grasa = float(input("ingrese los gramos de grasa: "))
-    
+    separador()
     suma = (proteina * 4) + (carbohidratos * 4) + (grasa * 9)
+    separador()
     print("el total de calorias es: ", suma)
+    separador()
+    print("gracias por usar la calculadora de calorias")
+    
+def separador():
+    print("----------------------------------------------------------------------")
+
     
 while True:
     try:
